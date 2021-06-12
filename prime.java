d781c82a46756601a663f79fe42daba0f4bc1917
indu-cloud <59249050+indu-cloud@users.noreply.github.com>
2021-06-12 18:15:08 +0530

    Update Prime.java

--- prime.java
+++ prime.java
@@ -1,1 +1,28 @@
 
+class Prime
+{
+    public static void main(String args[])
+    {
+      System.out.println("Enter the range:");
+      int n;
+      int count=0;
+      Scanner s=new Scanner(System.in);
+      n=s.nextInt();
+      for(int i=2;i<n;i++)
+      {
+          if(n%i==0)
+          {
+              count=count+1;
+          }
+      }
+      if(count==2)
+      {
+          System.out.println(n+"is a prime number");
+      }
+      else
+      {
+          System.out.println(n+ "is not a prime number");
+      }
+          
+    }
+}
