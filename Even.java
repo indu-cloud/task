e48198f99126690ebf408e618a7977d59bf4127c
indu-cloud <59249050+indu-cloud@users.noreply.github.com>
2021-06-12 18:36:28 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,14 @@
+class Even
+{
+    public static void main(String args[])
+    {
+      int n;
+      System.out.println("enter the number:");
+      Scanner s=new Scanner(System.in);
+      n=s.nextInt();
+      if(n%2==0)
+      {
+          System.out.println("Is is a even number");
+      }
+    }
+}
